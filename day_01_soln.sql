d72642d162645c40f4f5406ec1aa06ca2a4a2965
kyc_james <kayikchoi20@gmail.com>
2024-12-07 13:36:03 +1100

    Add day 1 soln.

---
+++ day_01_soln.sql
@@ -0,0 +1,27 @@
+SELECT
+    child.name,
+    json_extract_string(wish.wishes, '$.first_choice') AS primary_wish,
+    json_extract_string(wish.wishes, '$.second_choice') AS secondary_wish,
+    json_extract_string(wish.wishes, '$.colors[0]') AS favorite_color,
+    json_array_length(json_extract(wish.wishes, '$.colors')) AS color_count,
+    CASE 
+        WHEN catlog_1st.difficulty_to_make = 1 THEN 'Simple Gift'
+        WHEN catlog_1st.difficulty_to_make = 2 THEN 'Moderate Gift'
+        WHEN catlog_1st.difficulty_to_make >= 3 THEN 'Complex Gift'
+    END AS gift_complexity,
+    CASE 
+        WHEN catlog_1st.category = 'outdoor' THEN 'Outside Workshop'
+        WHEN catlog_1st.category = 'educational' THEN 'Learning Workshop'
+        ELSE 'General Workshop'
+    END AS workshop_assignment
+FROM
+    children AS child
+LEFT JOIN
+    wish_lists AS wish ON child.child_id = wish.child_id
+LEFT JOIN
+    toy_catalogue AS catlog_1st ON catlog_1st.toy_name = json_extract_string(wish.wishes, '$.first_choice')
+LEFT JOIN
+    toy_catalogue AS catlog_2nd ON catlog_2nd.toy_name = json_extract_string(wish.wishes, '$.second_choice')
+ORDER BY
+    child.name
+LIMIT 5;

d000573ff338bfd70703c8402aa88468f398adbc
kyc_james <kayikchoi20@gmail.com>
2024-12-09 00:03:04 +1100

    Update day_02_soln.sql

--- day_02_soln.sql
+++ day_02_soln.sql
@@ -4,7 +4,7 @@ WITH allowed_chars AS (
     SELECT id, value FROM letters_a
     UNION ALL 
     SELECT id, value FROM letters_b
-  ) combined
+  ) AS combined
   WHERE value BETWEEN 65 AND 90  -- UPPERCASE
      OR value BETWEEN 97 AND 122 -- lowercase 
      OR value IN (32,33,34,39,40,41,44,45,46,58,59,63) -- allowed special characters
